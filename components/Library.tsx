8c174588816e4a7274d790bde6197696f847a2aa
Kirti Kamal <kirtikamal718@gmail.com>
2024-01-14 15:55:17 +0530

    fixed build errors

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,7 +7,7 @@ import { Song } from "@/types";
 import useUploadModal from "@/hooks/useUploadModal";
 import { useUser } from "@/hooks/useUser";
 import useAuthModal from "@/hooks/useAuthModal";
-// import useSubscribeModal from "@/hooks/useSubscribeModal";
+import useSubscribeModal from "@/hooks/useSubscribeModal";
 import useOnPlay from "@/hooks/useOnPlay";
 
 import MediaItem from "./MediaItem";
@@ -22,7 +22,7 @@ const Library: React.FC<LibraryProps> = ({
   const { user, subscription } = useUser();
   const uploadModal = useUploadModal();
   const authModal = useAuthModal();
-  // const subscribeModal = useSubscribeModal();
+  const subscribeModal = useSubscribeModal();
 
   const onPlay = useOnPlay(songs);
 
--- types.ts
+++ types.ts
@@ -1,59 +1,72 @@
-import exp from 'constants';
 import Stripe from 'stripe';
 
-export interface UserDetails {
-    id: string;
-    first_name: string;
-    last_name: string;
-    full_name?: string;
-    avatar_url?: string;
-    billing_address?: Stripe.Address;
-    payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
-};
+export interface Song {
+  id: string;
+  user_id: string;
+  author: string;
+  title: string;
+  song_path: string;
+  image_path: string;
+}
 
 export interface Product {
-    id: string;
-    active?: boolean;
-    name?: string;
-    description?: string;
-    images?: string[];
-    metadata?: Stripe.Metadata;
-};
+  id: string;
+  active?: boolean;
+  name?: string;
+  description?: string;
+  image?: string;
+  metadata?: Stripe.Metadata;
+}
 
 export interface Price {
-    id: string;
-    product_id: string;
-    active?: boolean;
-    description?: string;
-    currency?: string;
-    type?: Stripe.Price.Type;
-    unit_amount: number;
-    interval?: Stripe.Price.Recurring.Interval;
-    interval_count?: number;
-    trail_period_days?: number | null;
+  id: string;
+  product_id?: string;
+  active?: boolean;
+  description?: string;
+  unit_amount?: number;
+  currency?: string;
+  type?: Stripe.Price.Type;
+  interval?: Stripe.Price.Recurring.Interval;
+  interval_count?: number;
+  trial_period_days?: number | null;
+  metadata?: Stripe.Metadata;
+  products?: Product;
+}
 
-    unit_amount_decimal: string;
-    product: Product;
-    metadata?: Stripe.Metadata;
-};
+export interface Customer {
+  id: string;
+  stripe_customer_id?: string;
+}
+
+export interface UserDetails {
+  id: string;
+  first_name: string;
+  last_name: string;
+  full_name?: string;
+  avatar_url?: string;
+  billing_address?: Stripe.Address;
+  payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
+}
+
+export interface ProductWithPrice extends Product {
+  prices?: Price[];
+}
 
 export interface Subscription {
-    id: string;
-    user_id: string;
-    price_id?: string;
-    quantity?: number;
-    status?: Stripe.Subscription.Status;
-    metadata?: Stripe.Metadata;
-    cancel_at_period_end?: boolean;
-    created: string;
-    current_period_end: string;
-    current_period_start: string;
-    ended_at?: string;
-    canceled_at?: string;
-    cancel_at?: string;
-    trail_end?: string;
-    trail_start?: string;
-    price?: Price;
-};
-
-
+  id: string;
+  user_id: string;
+  status?: Stripe.Subscription.Status;
+  metadata?: Stripe.Metadata;
+  price_id?: string;
+  quantity?: number;
+  cancel_at_period_end?: boolean;
+  created: string;
+  current_period_start: string;
+  current_period_end: string;
+  ended_at?: string;
+  cancel_at?: string;
+  canceled_at?: string;
+  trial_start?: string;
+  trial_end?: string;
+  prices?: Price;
+}
